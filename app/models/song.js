ef7ee4f9bca5437f0e6d40b03d37ae93faa4baba
Ed Gibbs <ed@edgibbs.com>
2020-07-04 10:04:15 -0700

    Pulls the songs

--- app/models/song.js
+++ app/models/song.js
@@ -1,11 +1,12 @@
 export default class Song {
   title = '';
   rating = 0;
-  band = '';
+  band = null;
 
-  constructor({ title, rating, band }) {
+  constructor({ title, rating, band }, relationships={}) {
     this.title = title;
     this.rating = rating;
     this.band = band;
+    this.relationships = relationships;
   }
 }
