4eaae33b32d6debdc31e0ad5a1948f9aef2f8bd5
Ed Gibbs <ed@edgibbs.com>
2020-07-04 19:06:55 -0700

    Added confirmation when navigating away from new band

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -6,6 +6,20 @@ export default class BandsNewController extends Controller {
   @service catalog;
   @service router;
 
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', transition => {
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          let leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (!leave) {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   @action
   async saveBand() {
     let band = await this.catalog.create('band', { name: this.name });
