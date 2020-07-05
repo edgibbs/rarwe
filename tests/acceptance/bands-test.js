d6e883c29a7f4aaf9d243f691ee6883a57e339fc
Ed Gibbs <ed@edgibbs.com>
2020-07-04 21:14:04 -0700

    More passing mirage acceptance tests

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, click, fillIn, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
@@ -16,5 +16,22 @@ module('Acceptance | Bands', function(hooks) {
     assert.equal(bandLinks.length, 2, 'All band links are rendered');
     assert.ok(bandLinks[0].textContent.includes('Radiohead'));
     assert.ok(bandLinks[1].textContent.includes('Long Distance Calling'));
+  });
+
+  test('Create a band', async function(assert) {
+    this.server.logging = true;
+    this.server.create('band', { name: 'Royal Blood' });
+
+    await visit('/');
+    await click('a[href="/bands/new"]');
+    await fillIn('input', 'Caspian');
+    await click('button');
+    await waitFor('p.text-center');
+
+    let bandLinks = document.querySelectorAll('.mb-2 > a');
+    assert.equal(bandLinks.length, 2)
+    assert.ok(bandLinks[0].textContent.includes('Royal Blood'));
+    assert.ok(bandLinks[1].textContent.includes('Caspian'));
+    assert.ok(document.querySelector('.border-b-4.border-purple-400').textContent.includes('Songs'), 'The Songs tab is active');
   });
 });
