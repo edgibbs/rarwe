780576cd570085404cda9c11d07c7a2e4362b56e
Ed Gibbs <ed@edgibbs.com>
2020-07-06 15:36:06 -0700

    Added option to show wait for seeing loading templates

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,12 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import Band from 'rarwe/models/band';
-import fetch from 'fetch';
 
 export default class BandsRoute extends Route {
   @service catalog;
 
-  model() {
+  async model() {
     return this.catalog.fetchAll('bands');
   }
 }
