c535f5775324ddf09b26e3bf87c3aa2938ce48b3
Ed Gibbs <ed@edgibbs.com>
2020-07-05 17:06:25 -0700

    Allow songs to be sorted

---
+++ tests/acceptance/songs-test.js
@@ -0,0 +1,23 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click, pauseTest } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Acceptance | Songs', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('Sort songs in various ways', async function(assert) {
+    let band = this.server.create('band', { name: 'Them Crooked Vultures' });
+    this.server.create('song', { title: 'Mind Eraser, No Chaser', rating: 2, band});
+    this.server.create('song', { title: 'Elephants', rating: 4, band});
+    this.server.create('song', { title: 'Spinning in Daffodils', rating: 5, band});
+    this.server.create('song', { title: 'New Fang', rating: 3, band});
+
+    await visit('/');
+    await click('[data-test-rr="band-link"]');
+
+    assert.dom('[data-test-rr="song-list-item"]:first-child').hasText('Elephants');
+    assert.dom('[data-test-rr="song-list-item"]:last-child').hasText('Spinning in Daffodils');
+  });
+});
